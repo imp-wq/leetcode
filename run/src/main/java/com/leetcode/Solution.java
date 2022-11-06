d9856ed46e2d3d18172d265e6768623fb0405486
imp-wq <imp-wq>
2022-11-06 23:11:42 +0800

    1.two sum, add another 2 approaches

--- run/src/main/java/com/leetcode/Solution.java
+++ run/src/main/java/com/leetcode/Solution.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.sql.Array;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Solution {
     @Test
@@ -26,19 +27,35 @@ public class Solution {
     //     return new int[]{};
     // }
 
-    public int[] twoSum(int[] numbers, int target) {
-        int start = 0, end = numbers.length - 1;
-        while (start < end) {
-            int sum;
-            if ((sum = numbers[start] + numbers[end]) == target) {
-                return new int[]{start + 1, end + 1};
+    // Approach 1: brute force
+    //public int[] twoSum(int[] numbers, int target) {
+    //    int start = 0, end = numbers.length - 1;
+    //    while (start < end) {
+    //        int sum;
+    //        if ((sum = numbers[start] + numbers[end]) == target) {
+    //            return new int[]{start + 1, end + 1};
+    //        }
+    //        if (sum < target) {
+    //            start++;
+    //        } else {
+    //            end--;
+    //        }
+    //    }
+    //    return new int[0];
+    //}
+
+    // Approach 2: Hashtable
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int value = nums[i];
+            int complement = target - value;
+            if (hashMap.containsKey(complement)) {
+                return new int[]{i, hashMap.get(complement)};
             }
-            if (sum < target) {
-                start++;
-            } else {
-                end--;
-            }
+            hashMap.put(value, i);
         }
+
         return new int[0];
     }
 }
